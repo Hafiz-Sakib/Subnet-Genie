d498d84de9f514a96b8a8705de82e25d6452329a
Hafiz_Sakib <hafizsakib5@gmail.com>
2025-01-23 12:01:33 +0600

    Refactor NormalSubnetResult component; simplify color logic for mobile and desktop views, enhance UI structure, and improve readability

--- src/NormalSubnetResult.js
+++ src/NormalSubnetResult.js
@@ -5,15 +5,6 @@ function NormalSubnetResult() {
   const [subnets, setSubnets] = useState([]);
   const navigate = useNavigate();
 
-  const cardColors = [
-    "bg-teal-500",
-    "bg-indigo-500",
-    "bg-cyan-500",
-    "bg-green-500",
-    "bg-pink-500",
-    "bg-blue-500",
-  ];
-
   function getSubnetPrefixLength(subnetMask) {
     let binarySubnetMask = subnetMask
       .split(".")
@@ -39,103 +30,107 @@ function NormalSubnetResult() {
         </h2>
 
         {subnets.length > 0 ? (
-          <div className="sm:hidden">
+          <>
             {/* Mobile Card View */}
-            <div className="grid grid-cols-1 gap-6">
-              {subnets.map((subnet, index) => (
-                <div
-                  key={index}
-                  className={`p-6 rounded-lg ${
-                    cardColors[index % cardColors.length]
-                  } text-white`}
-                >
-                  <h3 className="text-2xl font-semibold mb-4">
-                    {subnet.subnet}
-                  </h3>
-                  <p>
-                    <strong>Network Address:</strong> {subnet.networkAddress}/
-                    {getSubnetPrefixLength(subnet.subnetMask)}
-                  </p>
-                  <p>
-                    <strong>Subnet Mask:</strong> {subnet.subnetMask}
-                  </p>
-                  <p>
-                    <strong>Broadcast Address:</strong>{" "}
-                    {subnet.broadcastAddress}
-                  </p>
-                  <p>
-                    <strong>First Host:</strong> {subnet.firstHost}
-                  </p>
-                  <p>
-                    <strong>Last Host:</strong> {subnet.lastHost}
-                  </p>
-                </div>
-              ))}
+            <div className="sm:hidden">
+              <div className="grid grid-cols-1 gap-6">
+                {subnets.map((subnet, index) => (
+                  <div
+                    key={index}
+                    className={`p-6 rounded-lg ${
+                      index % 2 === 0 ? "bg-teal-200" : "bg-teal-400"
+                    } text-black`}
+                  >
+                    <h3 className="text-2xl font-semibold mb-4 text-black">
+                      {subnet.subnet}
+                    </h3>
+                    <p className="text-black">
+                      <strong>Network Address:</strong> {subnet.networkAddress}/
+                      {getSubnetPrefixLength(subnet.subnetMask)}
+                    </p>
+                    <p className="text-black">
+                      <strong>Subnet Mask:</strong> {subnet.subnetMask}
+                    </p>
+                    <p className="text-black">
+                      <strong>Broadcast Address:</strong>{" "}
+                      {subnet.broadcastAddress}
+                    </p>
+                    <p className="text-black">
+                      <strong>First Host:</strong> {subnet.firstHost}
+                    </p>
+                    <p className="text-black">
+                      <strong>Last Host:</strong> {subnet.lastHost}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
+
+            {/* Desktop Table View */}
+            <div className="hidden sm:block">
+              <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border-collapse table-auto">
+                  <thead>
+                    <tr className="bg-gray-100 text-left">
+                      <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
+                        Subnet
+                      </th>
+                      <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
+                        Network Address
+                      </th>
+                      <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
+                        Subnet Mask
+                      </th>
+                      <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
+                        Broadcast Address
+                      </th>
+                      <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
+                        First Host
+                      </th>
+                      <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
+                        Last Host
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {subnets.map((subnet, index) => (
+                      <tr
+                        key={index}
+                        className={`${
+                          index % 2 === 0 ? "bg-teal-200" : "bg-teal-100"
+                        }`}
+                      >
+                        <td className="py-4 px-6 sm:px-4 border-b text-lg text-gray-800">
+                          {subnet.subnet}
+                        </td>
+                        <td className="py-4 px-6 sm:px-4 border-b text-lg text-gray-800">
+                          {subnet.networkAddress}/
+                          {getSubnetPrefixLength(subnet.subnetMask)}
+                        </td>
+                        <td className="py-4 px-6 sm:px-4 border-b text-lg text-gray-800">
+                          {subnet.subnetMask}
+                        </td>
+                        <td className="py-4 px-6 sm:px-4 border-b text-lg text-gray-800">
+                          {subnet.broadcastAddress}
+                        </td>
+                        <td className="py-4 px-6 sm:px-4 border-b text-lg text-gray-800">
+                          {subnet.firstHost}
+                        </td>
+                        <td className="py-4 px-6 sm:px-4 border-b text-lg text-gray-800">
+                          {subnet.lastHost}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </>
         ) : (
           <p className="text-center text-gray-500 text-xl">
             No results available.
           </p>
         )}
-
-        <div className="hidden sm:block">
-          {/* Desktop Table View */}
-          <div className="overflow-x-auto">
-            <table className="min-w-full bg-white border-collapse table-auto">
-              <thead>
-                <tr className="bg-gray-100 text-left">
-                  <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
-                    Subnet
-                  </th>
-                  <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
-                    Network Address
-                  </th>
-                  <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
-                    Subnet Mask
-                  </th>
-                  <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
-                    Broadcast Address
-                  </th>
-                  <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
-                    First Host
-                  </th>
-                  <th className="py-4 px-6 sm:px-4 border-b text-lg font-semibold text-gray-800">
-                    Last Host
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {subnets.map((subnet, index) => (
-                  <tr
-                    key={index}
-                    className={`${cardColors[index % cardColors.length]}`}
-                  >
-                    <td className="py-4 px-6 sm:px-4 border-b text-lg text-white">
-                      {subnet.subnet}
-                    </td>
-                    <td className="py-4 px-6 sm:px-4 border-b text-lg text-white">
-                      {subnet.networkAddress}/
-                      {getSubnetPrefixLength(subnet.subnetMask)}
-                    </td>
-                    <td className="py-4 px-6 sm:px-4 border-b text-lg text-white">
-                      {subnet.subnetMask}
-                    </td>
-                    <td className="py-4 px-6 sm:px-4 border-b text-lg text-white">
-                      {subnet.broadcastAddress}
-                    </td>
-                    <td className="py-4 px-6 sm:px-4 border-b text-lg text-white">
-                      {subnet.firstHost}
-                    </td>
-                    <td className="py-4 px-6 sm:px-4 border-b text-lg text-white">
-                      {subnet.lastHost}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
 
         <div className="text-center mt-10">
           <button
