bb09f7b1f2ef311db7106d808c08ba6c9d67d24e
Hafiz_Sakib <hafizsakib5@gmail.com>
2026-05-04 12:01:46 +0600

    Fix import statement for NavBar component across multiple files and refactor code for better readability and consistency

--- src/Cidrrange.js
+++ src/Cidrrange.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import NavBar from "./Navbar";
+import NavBar from "./NavBar";
 import "./App.css";
 
 function isValidIP(ip) {
