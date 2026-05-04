cb95df386bf284a40985379214c91b16c19c87f0
Hafiz_Sakib <hafizsakib5@gmail.com>
2026-05-04 09:10:38 +0600

    fix: correct import path for NavBar component in WildcardMask

--- src/Wildcardmask.js
+++ src/Wildcardmask.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import NavBar from "./NavBar";
+import NavBar from "./Navbar";
 import "./App.css";
 
 function maskToInt(mask) {
